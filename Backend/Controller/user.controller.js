aeb2a81a5ad57dda2e5c6856d68ea53736d61cd1
Harsh Ninave <harshninave58@gmail.com>
2024-05-28 00:39:41 +0530

    userLogin route created

--- Backend/Controller/user.controller.js
+++ Backend/Controller/user.controller.js
@@ -2,6 +2,7 @@ import User from '../Model/userModel.js';
 import bcrypt from 'bcryptjs';
 
 export const signUp = async (req, res) => {
+  /*****************************Register user route*****************************/
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json({
@@ -33,5 +34,42 @@ export const signUp = async (req, res) => {
     res.status(500).json({
       error: 'Try again one more time',
     });
+  }
+};
+
+export const login = async (req, res) => {
+  /*****************************Login user route*****************************/
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      error: 'Email and password are required for login',
+    });
+  }
+
+  // Check if the email is already exists
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      error: 'Incorrect email',
+    });
+  }
+
+  // Check if the password is correct
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(400).json({
+      error: 'Incorrect password',
+    });
+  }
+
+  try {
+    res.status(200).json({
+      message: 'Login successfull',
+      user: { email: user.email, name: user.name, _id: user._id },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
--- Backend/Route/user.route.js
+++ Backend/Route/user.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { signUp } from '../Controller/user.controller.js';
+import { login, signUp } from '../Controller/user.controller.js';
 
 const router = express.Router();
 
+/*****************************Register user route*****************************/
 router.post('/signup', signUp);
+
+/*****************************Login user route*****************************/
+router.post('/login', login);
 
 export default router;
