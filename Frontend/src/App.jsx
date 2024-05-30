d8c12e656c040f2f7564e0f73dd873b9f2515d1b
Harsh Ninave <harshninave58@gmail.com>
2024-05-30 12:48:24 +0530

    Course route protected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import Home from './Home/Home';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Courses from './Courses/Courses';
 import Signup from './components/Signup';
 import Contact from './components/Contact';
 import { Toaster } from 'react-hot-toast';
+import { useAuth } from './Context/AuthProvider';
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <div className="bg-orange-50 h-screen object-cover w-full dark:bg-black dark:text-white">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Courses />} />
+        <Route path="/course" element={authUser ? <Courses /> : <Navigate to="/signup" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
---
+++ Frontend/src/Context/AuthProvider.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useState } from 'react';
+
+export const AuthContext = createContext();
+export default function AuthProvider({ children }) {
+  const initialAuthUser = localStorage.getItem('Users');
+  const [authUser, setAuthUser] = useState(
+    initialAuthUser ? JSON.parse(initialAuthUser) : undefined,
+  );
+  return <AuthContext.Provider value={[authUser, setAuthUser]}>{children}</AuthContext.Provider>;
+}
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
