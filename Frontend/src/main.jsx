fbd9c973db2ae3ac11a6156ac781e1dc0f714756
Harsh Ninave <harshninave58@gmail.com>
2024-12-10 20:34:12 +0530

    fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import AuthProvider from './Context/AuthProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <div className="dark:bg-black dark:text-white">
+      <div className="bg-orange-50 dark:bg-black dark:text-white">
         <App />
       </div>
     </AuthProvider>
