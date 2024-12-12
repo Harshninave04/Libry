39f3526e95a03a30138ecac75d28903c3a190b45
Harsh Ninave <harshninave58@gmail.com>
2024-12-12 20:06:25 +0530

    layout and grid fixed!!

--- Frontend/src/components/AvailBooks.jsx
+++ Frontend/src/components/AvailBooks.jsx
@@ -69,7 +69,7 @@ function AvailBooks() {
           <p className="dark:text-gray-500">Books you can explore for free</p>
         </div>
 
-        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {book.map((item) => (
             <Cards item={item} key={item.id} />
           ))}
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -21,35 +21,37 @@ function Course() {
 
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 bg-orange-50 dark:bg-black dark:text-white">
-        <div className="flex flex-col items-center justify-center text-center py-28">
-          <h1 className="text-2xl md:text-4xl font-semibold text-black dark:text-white">
-            Elite Collection: Unlock Knowledge with Our{' '}
-            <span className="md:bg-gradient-to-r md:from-yellow-400 md:to-orange-500 md:text-black text-2xl md:text-3xl md:rounded-lg md:p-2 md:shadow-md">
-              Premium Books
-            </span>{' '}
-            :)
-          </h1>
-          <p className="text-lg md:text-xl mt-8 text-slate-700 dark:text-slate-400">
-            Explore a curated selection of premium books across various genres and topics,
-            meticulously chosen to inspire, educate, and entertain. Whether you're seeking to expand
-            your professional skills, delve into captivating fiction, or discover new hobbies, our
-            exclusive collection offers something for every reader. Elevate your reading experience
-            with high-quality, expertly crafted content that promises to enrich your mind and spark
-            your imagination.
-          </p>
-          <Link to="/">
-            <a
-              className="btn text-white hover:bg-slate-700 px-4 text-base mt-8 dark:text-black dark:bg-white"
-              href="/">
-              Back
-            </a>
-          </Link>
-        </div>
-        <div className="grid grid-cols-1 md:grid md:grid-cols-3 pb-4">
-          {book.map((item) => (
-            <Cards item={item} key={item.id} />
-          ))}
+      <div className='bg-orange-50 dark:bg-black'>
+        <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 bg-orange-50 dark:bg-black dark:text-white">
+          <div className="flex flex-col items-center justify-center text-center py-28">
+            <h1 className="text-2xl md:text-4xl font-semibold text-black dark:text-white">
+              Elite Collection: Unlock Knowledge with Our{' '}
+              <span className="md:bg-gradient-to-r md:from-yellow-400 md:to-orange-500 md:text-black text-2xl md:text-3xl md:rounded-lg md:p-2 md:shadow-md">
+                Premium Books
+              </span>{' '}
+              :)
+            </h1>
+            <p className="text-lg md:text-xl mt-8 text-slate-700 dark:text-slate-400">
+              Explore a curated selection of premium books across various genres and topics,
+              meticulously chosen to inspire, educate, and entertain. Whether you're seeking to
+              expand your professional skills, delve into captivating fiction, or discover new
+              hobbies, our exclusive collection offers something for every reader. Elevate your
+              reading experience with high-quality, expertly crafted content that promises to enrich
+              your mind and spark your imagination.
+            </p>
+            <Link to="/">
+              <a
+                className="btn text-white hover:bg-slate-700 px-4 text-base mt-8 dark:text-black dark:bg-white"
+                href="/">
+                Back
+              </a>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 pb-4">
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </div>
         </div>
       </div>
     </>
