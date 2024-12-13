84cbf57a3cddea800bb6091339b61847a205efa1
Harsh Ninave <harshninave58@gmail.com>
2024-12-13 23:56:40 +0530

    #4

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'react-icons': path.resolve(__dirname, 'node_modules/react-icons'),
-    },
-  },
-  optimizeDeps: {
-    include: ['react-icons'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+      'react-icons': 'react-icons',
     },
   },
 });
